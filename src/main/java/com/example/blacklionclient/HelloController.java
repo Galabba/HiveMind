6100b6f53b04c9eb94871f97bde2c0a3b6be525f
ricky <rickyroma@gmail.com>
2025-05-28 17:57:49 +0200

    added Login Method

--- src/main/java/com/example/blacklionclient/HelloController.java
+++ src/main/java/com/example/blacklionclient/HelloController.java
@@ -2,32 +2,44 @@ package com.example.blacklionclient;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
+import org.w3c.dom.Text;
 
 import java.sql.*;
 
 public class HelloController {
     @FXML
     private Pane home, login;
+    @FXML
+    private TextField User, Pass;
+    @FXML
     private Button log_out;
+    public User curr_user;
     private static Statement statement;     //Classe per l'invio delle query
     private static ResultSet resultSet;     //Classe per l'output delle query
-
+    private static Connection connection;
     @FXML
-    protected void onLogIn() {          //cambio scena Login-->Home
-
-     /*  try {
-           statement.executeQuery("SELECT User, Password FROM dipendente");
+    protected void onLogIn() {          //Controllo Credenziali e Login
+        try {
+           statement = connection.createStatement();
+           statement.executeQuery("SELECT * FROM dipendente");
            resultSet = statement.getResultSet();
            while (resultSet.next()){
-               if(resultSet.getString("User")=="")
-
+               if(resultSet.getString("User").equals(User.getText()) && resultSet.getString("Password").equals(Pass.getText())){
                    home.setVisible(true);
-               login.setVisible(false);
+                   login.setVisible(false);
+                   curr_user= new User(resultSet.getString("User"), resultSet.getString("Password"),
+                           resultSet.getString("Nome"), resultSet.getString("Cognome"),
+                           resultSet.getString("Dipartimento"), resultSet.getString("Sede"));
+                   curr_user.setLogin(true);
+               }
            }
+
        } catch (SQLException e) {
            throw new RuntimeException(e);
-       }*/
+       }
     }
     @FXML
     protected void onLogOut(){          //cambio scena Home-->Login
@@ -35,7 +47,6 @@ public class HelloController {
         login.setVisible(true);
     }
     public static void DB_Connection(){
-        Connection connection = null;
         try {
             // Try per la connettività al DB
             Class.forName("com.mysql.cj.jdbc.Driver");
--- src/main/java/com/example/blacklionclient/User.java
+++ src/main/java/com/example/blacklionclient/User.java
@@ -1,5 +1,68 @@
 package com.example.blacklionclient;
 
 public class User {
+    private String username, passw, name, surname, depart, city;
+    private Boolean login =false;
 
+    public User(String username, String passw, String name, String surname, String depart, String city) {
+        this.username = username;
+        this.passw = passw;
+        this.name = name;
+        this.surname = surname;
+        this.depart = depart;
+        this.city = city;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassw() {
+        return passw;
+    }
+    public void setPassw(String passw) {
+        this.passw = passw;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getSurname() {
+        return surname;
+    }
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+    public String getDepart() {
+        return depart;
+    }
+    public void setDepart(String depart) {
+        this.depart = depart;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public Boolean getLogin() {
+        return login;
+    }
+    public void setLogin(Boolean login) {
+        this.login = login;
+    }
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", passw='" + passw + '\'' +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", depart='" + depart + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
 }
